d65398911779a589a50fd5c7943b6e4cf8035431
MJ Johnson <mjohnson4685@gmail.com>
2020-09-16 18:57:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,10 +32,8 @@ def monopoly_with_third_tier
 		{:one_piece_owned=>25
 	 }
  }
-
- }
+}
 #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
